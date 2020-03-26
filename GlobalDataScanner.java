2e260a8b52eac408551cb47bb47117fb1f2adc05
Anna Nilsson <annanilsson@Annas-MacBook-Pro-2.local>
2020-03-26 15:53:07 +0100

    Initial commit

---
+++ GlobalDataScanner.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
+    public class GlobalDataScanner
+    {
+        public static void main()
+        {
+            System.out.println("Hej!")
+            System.out.println("Wow"!)
+        }
+
+
+
+    }
+
+
